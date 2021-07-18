052af2a94a318ee0ca8c8cf76bbcc6b849add00c
Yurii_Hasiuk <Yurii.Hasiuk@externals.adidas.com>
2021-07-18 22:42:36 +0300

    Update config

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -1,16 +1,24 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'production',
   entry: path.resolve(__dirname, '..', 'src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, '../', 'dist'),
-    clean: true
+    path: path.resolve(__dirname, '../', 'dist')
   },
   plugins: [
     new HtmlWebpackPlugin({ template: path.resolve(__dirname, '..', 'src/index.html') }),
+      new CopyPlugin({
+          patterns: [
+              {
+                  from: path.resolve(__dirname, '..', "assets"),
+                  to: path.resolve(__dirname, '..', "dist/assets"),
+              },
+          ],
+      }),
   ],
   module: {
     rules: [
